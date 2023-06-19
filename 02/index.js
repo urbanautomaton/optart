6845e07cb99dc7514c18a4ba16cf964972c7b317
Simon Coffey <simon.coffey@futurelearn.com>
2023-06-19 22:30:31 +0100

    Render tiles with a generator pattern
    This abstracts the hard-coded rotation from the last commit, and uses a
    generator kernel to pick the tile label for a given tile.

--- 02/index.js
+++ 02/index.js
@@ -1,6 +1,22 @@
 const truchet = document.getElementById("truchet-image");
 const context = truchet.getContext("2d");
-const tileSize = 100;
+truchet.width = 500;
+truchet.height = 500;
+const tileSize = 20;
+
+const rotations = {
+  a: 0,
+  b: Math.PI / 2,
+  c: Math.PI,
+  d: (Math.PI * 3) / 2,
+};
+
+const generator = [
+  ["b", "a"],
+  ["c", "d"],
+];
+const generatorX = generator[0].length;
+const generatorY = generator.length;
 
 const drawBase = (x, y, t) => {
   const delta = tileSize * (0.75 - 0.5 * t);
@@ -14,8 +30,28 @@ const drawBase = (x, y, t) => {
   context.fill();
 };
 
-drawBase(0, 0, 0);
-context.translate(150, 50);
-context.rotate(Math.PI / 2);
-context.translate(-150, -50);
-drawBase(100, 0, 0);
+const drawTile = (label, x, y, t) => {
+  const rotation = rotations[label];
+  const centerX = x + tileSize / 2;
+  const centerY = y + tileSize / 2;
+
+  if (rotation > 0) {
+    context.translate(centerX, centerY);
+    context.rotate(rotation);
+    context.translate(-centerX, -centerY);
+  }
+  drawBase(x, y, t);
+  if (rotation > 0) {
+    context.translate(centerX, centerY);
+    context.rotate(-rotation);
+    context.translate(-centerX, -centerY);
+  }
+};
+
+for (var i = 0; i <= 25; i++) {
+  for (var j = 0; j <= 25; j++) {
+    const tileLabel = generator[j % generatorY][i % generatorX];
+
+    drawTile(tileLabel, i * tileSize, j * tileSize, Math.random());
+  }
+}
